de8d34e53c1a1285b23c858c14ec49d4f500ac0e
apostolisdiam <52792179+apostolisdiam@users.noreply.github.com>
2019-07-12 18:52:59 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="netassignment.WebApiApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(netassignment.Startup))]
+
+namespace netassignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
