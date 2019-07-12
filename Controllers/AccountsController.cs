fd5abf12ffef0e77a12e7c8f15075d760908c8f1
apostolisdiam <52792179+apostolisdiam@users.noreply.github.com>
2019-07-12 18:33:22 +0300

    Add files via upload

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using netassignment.Models;
+
+namespace netassignment.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        static readonly IAccountRepository repository = new AccountRepository();
+
+        public Account PostAccount (Account item)
+        {
+            item = repository.Add(item);
+            return item;
+        }
+        
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return repository.GetAll();
+        }
+
+        public Account GetAccount(int id) //for specific ID with /id
+        {
+            Account item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+            
+    }
+}
